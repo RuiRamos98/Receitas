5ebb3896f5a47d228e3097a0183450cfb42af48b
1704711 <xadowknight@gmail.com>
2023-05-23 10:34:11 +0100

    Mudar nomes fragmentos

--- app/src/main/java/com/example/receitas/Fragmento_sobre.kt
+++ app/src/main/java/com/example/receitas/Fragmento_sobre.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.receitas.databinding.FragmentFirstBinding
+import com.example.receitas.databinding.FragmentoSobreBinding
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class Fragmento_sobre : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentoSobreBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentoSobreBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -32,8 +32,8 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSobre.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 
