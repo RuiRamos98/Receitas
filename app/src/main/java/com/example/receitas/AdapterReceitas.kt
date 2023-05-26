54f461fd43d10f1df4d064f50a2b4321ed474b4d
1704711 <xadowknight@gmail.com>
2023-05-26 09:18:43 +0100

    Criar adaptador Receitas(faz o "interface" com o RecycleView)

---
+++ app/src/main/java/com/example/receitas/AdapterReceitas.kt
@@ -0,0 +1,25 @@
+package com.example.receitas
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class AdapterReceitas: RecyclerView.Adapter<AdapterReceitas.ViewHolderReceita>() {
+
+    inner class ViewHolderReceita(itemView: View) : ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderReceita {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderReceita, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
