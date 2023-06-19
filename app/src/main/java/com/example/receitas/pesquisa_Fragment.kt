00576bcd8a416e2974f4748c3ebd1340d7e646d0
ecXKei <xadowknight@gmail.com>
2023-06-19 06:06:12 +0100

    Implementação do Pesquisar no respectivo fragmento e habilitação da navegação para o mesmo

--- app/src/main/java/com/example/receitas/pesquisa_Fragment.kt
+++ app/src/main/java/com/example/receitas/pesquisa_Fragment.kt
@@ -1,62 +1,100 @@
 package com.example.receitas
 
+import android.database.Cursor
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.receitas.databinding.FragmentPesquisaBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ID_LOADER_RECEITAS_PESQUISA = 0
+class pesquisa_Fragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [pesquisa_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class pesquisa_Fragment : Fragment() {
+    private var _binding: FragmentPesquisaBinding? = null
+    private val binding get() = _binding!!
+    private var adapterReceitas: AdapterReceitasPesquisa? = null
 
-    var receitaSelecionado: Receita?=null
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    var receitaSelecionado: Receita? = null
+        set(value) {
+            field = value
         }
-    }
+
+    var textPesquisa: String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pesquisa_, container, false)
+    ): View? {        _binding = FragmentPesquisaBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment pesquisa_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            pesquisa_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapterReceitas = AdapterReceitasPesquisa(this)
+        binding.recyclerViewPesquisa.adapter = adapterReceitas
+        binding.recyclerViewPesquisa.layoutManager = LinearLayoutManager(requireContext())
+
+        val loader = LoaderManager.getInstance(this)
+        loader.initLoader(ID_LOADER_RECEITAS_PESQUISA, null, this)
+
+        textPesquisa = binding.TextInputPesquisa.text.toString()
+
+        binding.TextInputPesquisa.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Nenhuma ação necessária antes da alteração do texto
             }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Atualizar o texto de pesquisa sempre que houver uma alteração no TextInput
+                textPesquisa = s.toString()
+                restartLoader()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Nenhuma ação necessária após a alteração do texto
+            }
+        })
+
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        val selection = "${TabelaReceitas.CAMPO_NOME} LIKE ?"
+        val selectionArgs = arrayOf("%$textPesquisa%")
+        return CursorLoader(
+            requireContext(),
+            ReceitasContentProvider.ENDERECO_RECEITA,
+            TabelaReceitas.CAMPOS,
+            selection,
+            selectionArgs,
+            TabelaReceitas.CAMPO_NOME
+        )
+
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        if (adapterReceitas != null) {
+            adapterReceitas!!.cursor = null
+        }
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        adapterReceitas!!.cursor = data
+    }
+
+    private fun restartLoader() {
+        LoaderManager.getInstance(this).restartLoader(ID_LOADER_RECEITAS_PESQUISA, null, this)
     }
 }
